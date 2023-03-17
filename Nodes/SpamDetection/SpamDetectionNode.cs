02f60f1bd85b2576e528bfdadead5acbd1714f56
jr00t <joanas@gmail.com>
2023-03-17 15:06:18 -0400

    SPAM detection using Machine Learning

--- Nodes/SpamDetection/SpamDetectionNode.cs
+++ Nodes/SpamDetection/SpamDetectionNode.cs
@@ -1,1 +1,40 @@
-// TODO
+using NodeBlock.Engine;
+using NodeBlock.Engine.Attributes;
+using NodeBlock_Plugin_MachineLearning;
+using System;
+
+namespace NodeBlock.Plugin.MachineLearning.Nodes.SpamDetection
+{
+
+    [NodeDefinition("SpamDetectionNode", "Spam Detection", NodeTypeEnum.Function, "Machine Learning")]
+    [NodeGraphDescription("Predict if the text is spam or not")]
+    public class SpamDetectionNode : Node
+    {
+        public SpamDetectionNode(string id, BlockGraph graph)
+                : base(id, graph, typeof(SpamDetectionNode).Name)
+        {
+            InParameters.Add("testString", new NodeParameter(this, "testString", typeof(string), true));
+
+            OutParameters.Add("output", new NodeParameter(this, "output", typeof(bool), false));
+        }
+
+        public override bool CanBeExecuted => true;
+
+        public override bool CanExecute => true;
+
+        public override bool OnExecution()
+        {
+            string testString = InParameters["testString"].GetValue().ToString();
+            SpamModel.ModelInput sampleData = new SpamModel.ModelInput()
+            {
+                Col0 = InParameters["testString"].GetValue().ToString(),
+            };
+
+            SpamModel.ModelOutput result = SpamModel.Predict(sampleData);
+
+            OutParameters["output"].SetValue((int.Parse(result.PredictedLabel) == 1) ? true : false);
+
+            return true;
+        }
+    }
+}
